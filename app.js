dfddb5be9ae8f67063fc90d7010b1408fd512d72
byasaa <abi.vardhana@outlook.com>
2020-08-11 21:59:47 +0700

    Update: Static Public

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+
 app.use(cors())
 app.use('/', require('./src/routes'))
 
